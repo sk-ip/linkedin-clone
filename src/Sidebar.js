c4fa0a309c63e390409ca87c5356fffda1a402b8
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2021-09-11 13:06:05 +0000

    src: sidebar: Add sidebar component and css file

---
+++ src/Sidebar.js
@@ -0,0 +1,46 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
+    return (
+        <div className="sidebar">
+            <div className="sidebar__top">
+                <img src="" alt=""/>
+                <Avatar className="sidebar__avatar" />
+                <h2>Sudhanshu Kumar</h2>
+                <h4>sudhanshukumar5459@gmail.com</h4>
+            </div>
+
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar__statNumber">2,543</p>
+                </div>
+                <div className="sidebar__stat">
+                    <p>Views on post</p>
+                    <p className="sidebar__statNumber">2,448</p>
+                </div>
+            </div>
+
+            <div className="sidebar__bottom">
+                <p>recent</p>
+                {recentItem("reactjs")}
+                {recentItem("programming")}
+                {recentItem("software engineering")}
+                {recentItem("design")}
+                {recentItem("developer")}
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
