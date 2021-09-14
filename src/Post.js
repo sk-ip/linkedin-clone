df52058e7ed8ab368a3ba86c785f58c551b7d577
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2021-09-14 11:21:17 +0000

    src: Post: Show avatar image

--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,5 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react'
+import React, { forwardRef } from 'react'
 import './Post.css';
 import InputOption from './InputOption';
 import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
@@ -7,11 +7,11 @@ import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
 import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
 import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
 
-function Post({name, description, message, photoUrl}) {
+const Post = forwardRef(({name, description, message, photoUrl}, ref) => {
     return (
-        <div className="post">
+        <div ref={ref} className="post">
             <div className="post__header">
-                <Avatar />
+                <Avatar src={photoUrl}>{name[0]}</Avatar>
                 <div className="post__info">
                     <h2>{name}</h2>
                     <p>{description}</p>
@@ -30,7 +30,7 @@ function Post({name, description, message, photoUrl}) {
             </div>
         </div>
     )
-}
+})
 
 
 export default Post
