afb0466a65d1807492818b02fe0db8f5c6831904
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2021-09-14 11:21:55 +0000

    src: headerOption: Show avatar image

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import './HeaderOption.css'
 import {Avatar} from '@material-ui/core';
+import { selectUser } from './features/userSlice';
+import { useSelector } from 'react-redux';
 
-function HeaderOption({avatar, Icon, title}) {
+function HeaderOption({avatar, Icon, title, onClick}) {
+
+    const user = useSelector(selectUser);
+
     return (
-        <div className="headerOption">
+        <div onClick={onClick} className="headerOption">
             {Icon && <Icon className="headerOption__icon" />}
             {avatar && (
-                <Avatar className="headerOption__icon" src={avatar}/>
+                <Avatar className="headerOption__icon" src={user?.photoUrl}>{user?.email[0]}</Avatar>
             )}
             <h3 className="headerOption__title">{title}</h3>
         </div>
