90c913eea903f101a0355dc0716a7f40af21d68b
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2021-09-14 11:23:45 +0000

    src: app: Update app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import { useDispatch } from 'react-redux';
+import Login from './Login';
+import { auth } from './firebase';
+import { login, logout } from './features/userSlice';
+import Widgets from './Widgets';
 
 function App() {
+  
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        // user is logged in
+        dispatch(login({
+          displayName: userAuth.displayName,
+          email: userAuth.email,
+          uid: userAuth.uid,
+          photoUrl: userAuth.photoURL,
+        }))
+      } else {
+        // user not logged out
+        dispatch(logout());
+      }
+    })
+  })
+
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          <Widgets />
+        </div>
+      )}
     </div>
   );
 }
