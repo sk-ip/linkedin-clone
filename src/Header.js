7d092097140016463602e2669db81e7b5bf993b7
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2021-09-14 11:22:32 +0000

    src: header: Add avatar and logout feature

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
 import HeaderOption from './HeaderOption';
@@ -7,8 +7,18 @@ import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import ChatIcon from '@material-ui/icons/Chat';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import { useDispatch } from 'react-redux';
+import { logout } from './features/userSlice';
+import { auth } from './firebase'
 
 function Header() {
+    const dispatch = useDispatch();
+
+    const logoutOfApp = () => {
+        dispatch(logout());
+        auth.signOut();
+    }
+
     return (
         <div className="header">
 
@@ -27,6 +37,7 @@ function Header() {
                 <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
                 <HeaderOption Icon={ChatIcon} title="Messaging"/>
                 <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+                <HeaderOption avatar={true} title="me" onClick={logoutOfApp} />
             </div>
         </div>
     )
